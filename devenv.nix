e79fe7444cc6324d58c33327318007f8b6a3624d
Rutger <rutgerdj@users.noreply.github.com>
2025-04-17 11:20:45 +0200

    Add devenv config

---
+++ devenv.nix
@@ -0,0 +1,26 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+{
+  cachix.enable = false;
+
+  languages.ansible.enable = true;
+  packages = with pkgs; [
+    just
+  ];
+
+  pre-commit.hooks = {
+    vault-encrypted = {
+      enable = true;
+      name = "Ensure vars/vault.yml is ansible-vault encrypted";
+      entry = "${pkgs.bash}/bin/bash scripts/vault_pre_commit.sh";
+      language = "script";
+      files = "\\.ya?ml$";
+      pass_filenames = false;
+    };
+  };
+}
