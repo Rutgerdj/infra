4ec28be471c3da89ec38bb007494b400f6215561
Rutger <rutgerdj@users.noreply.github.com>
2025-06-01 16:49:22 +0200

    Update NixOS configuration to include inputs in immich-server and add imports in home.nix

--- nixos/hosts/immich-server/home.nix
+++ nixos/hosts/immich-server/home.nix
@@ -17,6 +17,11 @@
   # release notes.
   home.stateVersion = "24.11"; # Please read the comment before changing.
 
+  imports = [
+    ../../modules/git/default.nix
+    ../../modules/zsh/default.nix
+  ];
+
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
